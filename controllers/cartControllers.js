0094925bcb2e60964720b2b0936b8f5bf5d60a19
Samir Hamid <shamid@bluewin.ch>
2023-11-16 15:01:28 +0100

    small fixes in cartControllers

--- controllers/cartControllers.js
+++ controllers/cartControllers.js
@@ -1,14 +1,34 @@
 import tempDB from "../tempDB.json" assert { type: 'json' }
-import { readFile, writeFile } from "fs/promises"
+import { access, constants, readFile, writeFile } from "fs/promises"
 import path from "path"
 import { fileURLToPath } from 'url'
 import { errorHandler } from "../middleware/errorHandler.js"
+
+const railwayPath = path.resolve(process.env.RAILWAY_VOLUME_MOUNT_PATH, 'tempDB.json')
+console.log('First try Railway Path: ', railwayPath)
+
+const checkOrCreateDB = async () => {
+    try {
+        const doesDBExist = await access(railwayPath, constants.F_OK)
+        console.log('Does DB exist: ', doesDBExist)
+    } catch (error) {
+        
+        const initialData = {
+            "cart": [],
+            "checkoutStatus": false
+        }
+        await writeFile(railwayPath, JSON.stringify(initialData))
+    }
+    console.log('Should now exist: ', railwayPath)
+}
+checkOrCreateDB()
+// const filePath = path.resolve('./', 'tempDB.json')
+const filePath = railwayPath
 
 
 export const startCheckout = async (req, res) => {
     try {
         console.log(req.body)
-        const filePath = path.resolve('./', 'tempDB.json')
         const file = await readFile(filePath, 'utf-8')
         const fileData = JSON.parse(file)
         const dataCartItems = fileData.cart
@@ -29,7 +49,6 @@ export const startCheckout = async (req, res) => {
 
 export const getCartItems = async (req, res) => {
     try {
-    const filePath = path.resolve('./', 'tempDB.json')
     const file = await readFile(filePath, 'utf-8')
     const fileData = JSON.parse(file)
     const dataCartItems = fileData.cart
@@ -49,7 +68,6 @@ export const getCartItems = async (req, res) => {
 export const postCartItems = async (req, res) => {
     try {
         console.log(`Req.body: ${req.body}`)
-        const filePath = path.resolve('./', 'tempDB.json')
         console.log(filePath)
         const file = await readFile(filePath, 'utf-8')
         const fileData = JSON.parse(file)
@@ -73,7 +91,6 @@ export const postCartItems = async (req, res) => {
 export const removeCartItems = async (req, res) => {
     try {
         console.log(req.body)
-        const filePath = path.resolve('./', 'tempDB.json')
         console.log(filePath)
         const file = await readFile(filePath, 'utf-8')
         const fileData = JSON.parse(file)
@@ -100,7 +117,6 @@ export const removeCartItems = async (req, res) => {
 
 export const deleteCartItems = async (req, res) => {
     try {
-        const filePath = path.resolve('./', 'tempDB.json')
         console.log(filePath)
         const file = await readFile(filePath, 'utf-8')
         const fileData = JSON.parse(file)
